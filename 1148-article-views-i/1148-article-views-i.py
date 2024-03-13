5c1c91441a52468dc7521a03f88b18373a628484
Dr. Baisakhi Mal <131129333+bmalphys@users.noreply.github.com>
2024-03-13 13:54:19 +0530

    Time: 920 ms (5.05%), Space: 66.2 MB (70.32%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df1 = pd.DataFrame()
+    df1['id'] = views[views['author_id'] == views['viewer_id']][['author_id']]
+    return df1.drop_duplicates().sort_values(by = ['id'])
