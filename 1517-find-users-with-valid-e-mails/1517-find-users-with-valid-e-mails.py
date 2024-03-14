12309f1ba431d36873c00360cba8274f2e902868
Dr. Baisakhi Mal <131129333+bmalphys@users.noreply.github.com>
2024-03-14 22:39:59 +0530

    Time: 925 ms (5.17%), Space: 66.6 MB (14.31%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
-    allowed_chars = set("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-.")
-    users['mail'] = users['mail'].apply(lambda x: 'wrong' if 
+        allowed_chars = set("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-.")
+        users['mail'] = users['mail'].apply(lambda x: 'wrong' if 
                                         (x.rsplit("@", 1)[-1] != "leetcode.com" or
                                          set(x.rsplit("@", 1)[0]).issubset(allowed_chars) == False or
                                          x.rsplit("@", 1)[0][0].isalpha() == False) else x)
-    users = users.drop(users[users['mail'] == 'wrong'].index)
-    return users
+        users = users.drop(users[users['mail'] == 'wrong'].index)
+        return users
+    
