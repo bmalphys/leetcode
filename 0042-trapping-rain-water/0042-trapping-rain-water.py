5386faed122cc2a8f4fb8ab0b76634c5bb84d610
Dr. Baisakhi Mal <131129333+bmalphys@users.noreply.github.com>
2024-04-12 18:34:30 +0530

    Time: 86 ms (97.47%), Space: 18.3 MB (98.1%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        ans = 0
+        st = []
+        for r, x in enumerate(height):
+            while st and height[st[-1]] < x:
+                m = st[-1]
+                st.pop()
+                if not st: break
+                l = st[-1]
+                h = min(x, height[l]) - height[m]
+                w = r - l - 1
+                ans += h * w
+            st.append(r)
+        return ans     
