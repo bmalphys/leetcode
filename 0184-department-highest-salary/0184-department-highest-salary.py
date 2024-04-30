fcb2a55565f93c055b2bd1f8186ea190a96b4e3d
Dr. Baisakhi Mal <131129333+bmalphys@users.noreply.github.com>
2024-04-30 16:16:00 +0530

    Time: 797 ms (34.77%), Space: 68.6 MB (7.41%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -6,8 +6,4 @@ def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) 
     highest_salary = highest_salary.reset_index(drop = True)
     result = highest_salary[['name_department', 'name_employee', 'salary']].rename(columns={'name_department':'Department', 'name_employee': 'Employee', 'salary': 'Salary'})
     return result
-
-    result.columns = ['Department','Employee', 'Salary']
-
-    return resul
     
